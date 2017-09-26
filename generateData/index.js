2942ab4572b18a4d26c076dfc607de60cf39a32c
nezizzi <zizzi.n@husky.neu.edu>
2017-09-26 19:02:03 -0400

    in class!

--- generateData/index.js
+++ generateData/index.js
@@ -1,3 +1,35 @@
 var svg = d3.select('svg');
 
 /* Your code goes here */
+/*var array = [ 'milk', 'eggs', 'bread', 'butter'];
+ console.log(array[0]);
+
+ var student = { 'name': 'Nicole', 'job': 'student', 'age': 25};
+
+ console.log(student.age);
+
+ var classroom = [{ 'name': 'Nicole', 'job': 'student', 'age': 25},
+     { 'name': 'Nicole1', 'job': 'student1', 'age': 26},
+     { 'name': 'Nicole2', 'job': 'student2', 'age': 27}
+ ];
+
+console.log(classroom);
+
+console.log(classroom[1]);
+
+console.log(classroom[1].age);*/
+
+
+myCircle = {'cx':100 ,'cy':200, 'r':20, 'color':'blue'};
+
+//data binding !
+var myArray = [myCircle];
+
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+    
+
+})
