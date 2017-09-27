10daa7ddf860990135eba34e72be741bc0496584
nezizzi <zizzi.n@husky.neu.edu>
2017-09-26 20:40:29 -0400

    from csv

--- fromCsv/index.js
+++ fromCsv/index.js
@@ -2,6 +2,27 @@ var svg = d3.select('svg');
 
 /* Your code goes here */
 
+d3.csv('./data.csv', function(dataIn){
+    console.log(dataIn);
+
+    svg.selectAll('circle')
+        .data(dataIn)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d){
+            return d.x;
+        })
+        .attr('cy', function(d){
+            return d.y
+        })
+        .attr('r', function(d){
+            return d.r
+        })
+        .attr('fill', function(d){
+            return d.fill
+        });
+
+});
 
 
 
