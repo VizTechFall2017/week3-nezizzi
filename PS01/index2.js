312fd6ede4a228a5b612c194f6c64f78bde11922
nezizzi <zizzi.n@husky.neu.edu>
2017-10-01 13:49:53 -0400

    rect attempt

--- PS01/index2.js
+++ PS01/index2.js
@@ -6,7 +6,6 @@ for ( i =0; i < 1111; i++ ){
     var dizzy = {x1: 1000 * Math.random(), y1: 1000* Math.random(), x2: 500, y2: 500};
     myArray.push(dizzy);
 }
-console.log(myArray);
 
 svg.selectAll('RADIAL')
     .data(myArray).enter()
@@ -24,3 +23,42 @@ svg.selectAll('RADIAL')
         return d.y2
     })
     .attr('stroke','purple');
+
+
+myArray2 = [];
+
+var horz = 10;
+var vert= 10;
+var Ht = 10;
+var wd = 10;
+
+
+for( i =0; i < 111; i++){
+    var rect = {x: horz  , y: vert , height: Ht, width: wd};
+    horz = horz + 1;
+    vert= vert + 1;
+    Ht = Ht + 1;
+    wd = wd + 1;
+    console.log(vert);
+    myArray2.push(rect);
+}
+
+console.log(myArray2);
+
+svg.selectAll('ORTHOGONAL')
+    .data(myArray2)
+    .enter()
+    .append('rect')
+    .attr('cx', function(d){
+        return d.x
+    })
+    .attr('cy', function(d){
+        return d.y
+    })
+    .attr('height', function(d){
+        return d.height
+    })
+    .attr('width', function(d){
+        return d.width
+    })
+    .attr('fill', 'grey');
