f4c6d5739de4684746ee06c30f1af10b6088c6fa
nezizzi <zizzi.n@husky.neu.edu>
2017-09-30 23:11:57 -0400

    some work

--- PS01/index.js
+++ PS01/index.js
@@ -1,3 +1,11 @@
 var svg = d3.select('svg');
 
-/* Your code goes here */
+randomArray = [];
+
+for (i=0; i < 111; i++ ){
+    lines = {x1:100*Math.random(), y1: Math.random(), x2:500, y2:500};
+    randomArray.push(lines);
+}
+
+console.log(randomArray);
+
