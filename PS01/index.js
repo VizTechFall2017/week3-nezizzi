2d55cf01ef74fe512a835858619598bfd95f2d70
nezizzi <zizzi.n@husky.neu.edu>
2017-09-30 23:22:39 -0400

    start lines ps01

--- PS01/index.js
+++ PS01/index.js
@@ -2,9 +2,9 @@ var svg = d3.select('svg');
 
 randomArray = [];
 
-for (i=0; i < 111; i++ ){
-    lines = {x1:100*Math.random(), y1: Math.random(), x2:500, y2:500};
-    randomArray.push(lines);
+for ( i =0; i < 111; i++ ){
+    var dizzy = {x1: 100 * Math.random(), y1: 100* Math.random(), x2: 500, y2: 500};
+    randomArray.push(dizzy);
 }
 
 console.log(randomArray);
